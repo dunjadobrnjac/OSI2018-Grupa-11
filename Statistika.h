fddfaba76e6305ae2c4e92920af79c4de3b8dfe7
VukLalovic <vukinjo@gmail.com>
2018-12-30 22:14:10 +0100

    Statistika.h

---
+++ Statistika.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <iostream>
+void statistika(int, int);
+void IspisiStatistiku(struct Stats);
+void PisiUStatistiku(struct Stats);
+void UmetniTrenutneBodove(int, struct Stats, int);
+void PisiUCSVFajl(struct Stats);
+void BubbleSort(struct Stats, int);
+std::string Vrijeme(void);
+
+
+struct Stats {
+	int brojIgre;
+	std::string datum;
+	int brojBodova;
+};
