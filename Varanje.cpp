8290c6c1e6d18f9508b5907858c45fd964eb9de1
vladanmalinic <vlatci123@gmail.com>
2019-01-06 10:45:41 +0100

    Onemogućavanje pobjede

--- Varanje.cpp
+++ Varanje.cpp
@@ -1,28 +1,83 @@
-#include "Varanje.h"
-
+#include "Varanje.h"
+#include <iostream>
+#include <fstream>
 int funkcijaKakoOdigrati(Korisnik k)
 {
-	//return 2;
+
 	if (k.brojBodova < 0)
 	{
-		k.brojUlozenihBodova -= k.brojBodova;
-		k.brojBodova = 0;
+		
+		return 2;
 	}
-	int brojIgranja = 0;
-	if (brojIgranja % 20 < 18)
+	int referentniBrojBodova = 0;
+	std::ifstream datoteka;
+	datoteka.open("ReferentniBrojBodova.txt");
+	if (datoteka.is_open())
 	{
-		if (k.brojBodova > k.brojUlozenihBodova)
-			return 2;//sigurno mora pogoditi
-		else if (k.brojBodova > k.brojUlozenihBodova*0.6 && k.brojBodova < k.brojUlozenihBodova)
-			return 1;//prepusten sreci
-		else
-			return 0;//sigurno gubi
+		datoteka >> referentniBrojBodova;
+		datoteka.close();
 	}
+	/*int brojIgranja = 0;
+	if (brojIgranja % 20 < 18)
+	{*/
+	if (k.brojBodova > referentniBrojBodova)
+		return 0;//sigurno gubi
+	else if ((k.brojBodova < referentniBrojBodova) && (k.brojBodova > (referentniBrojBodova*0.6)))
+		return 1;//prepusten sreci
+	else
+		return 2;//sigurno mora pogoditi
+	/*}
 	else
 	{
-		if (k.brojBodova < 0.7*k.brojUlozenihBodova)
+		if (k.brojBodova < 0.7*referentniBrojBodova)
 			return 1;//prepusten sreci
 		else
 			return 0;//sigurno gubi
+	}*/
+}
+
+bool varanjePriprema(Korisnik k)
+{
+	int i = 0, datoteckiInt = 0;
+	std::cout << u8"Za koju igricu želite kupiti/preuzeti kljuc? (1/2/3/4)" << std::endl;
+	std::cin >> i;
+	std::ifstream datoteka;
+	datoteka.open("prviPogodak.txt");
+	if (datoteka.is_open())
+	{
+		datoteka >> datoteckiInt;
+		datoteka.close();
 	}
+	if (!datoteckiInt && k.brojBodova > 69)
+	{
+		std::ofstream datoteka;
+		datoteka.open("prviPogodak.txt");
+		if (datoteka.is_open())
+		{
+			datoteka << 1;
+			std::ofstream trenutniBrojBodova;
+			trenutniBrojBodova.open("referentniBrojBodova.txt");
+			if (trenutniBrojBodova.is_open())
+			{
+				trenutniBrojBodova << k.brojBodova;
+				trenutniBrojBodova.close();
+			}
+			datoteka.close();
+		}
+	}
+	std::ifstream datotekka;
+	datotekka.open("prviPogodak.txt");
+	if (datotekka.is_open())
+	{
+		datotekka >> datoteckiInt;
+		datotekka.close();
+	}
+	if (datoteckiInt)
+		k.ucitajKljuc(i);
+	else
+	{
+		std::cout << u8"Još uvijek niste osvojili prvih 70 bodova, pa ne možete preuzeti kljuc" << std::endl;
+		sleep(3000);
+	}
+	return true;
 }
--- Varanje.h
+++ Varanje.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Korisnik.h"
+#include "TrecaIgra.h"
 
 int funkcijaKakoOdigrati(Korisnik);
- 
+bool varanjePriprema(Korisnik);
